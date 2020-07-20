68e8684c239ffe4c2c281be7bad03149d94c2d19
mollyincali <mollyehardy@gmail.com>
2020-07-20 11:59:47 -0700

    Updating with graph and group success

--- eda_code.py
+++ eda_code.py
@@ -21,7 +21,7 @@ sbac['Students Tested'] = sbac['Students Tested'].astype(float)
 sbac['Percentage Standard Met and Above'] = sbac['Percentage Standard Met and Above'].astype(float)
 
 #import districts
-district_code = pd.read_csv("districts.csv")
+district = pd.read_csv("districts.csv")
 district = district.iloc[:, [0, 1, 5, 6]]
 
 #pull only top 10 county
@@ -36,16 +36,36 @@ top10 = top10[top10['District Code'] != 0]
 full = sbac.merge(top10, how = 'inner')
 full = full[full['Subgroup ID'] == 1]
 
-#Below worked! Look over again
-group_test = top10all.groupby(['District Code', 'Grade']).agg({'Students Tested':'sum', 
-                                                               'Percentage Standard Met and Above': 'mean', 
-                                                               'County Name':'unique',
-                                                               'District Name':'unique',
-                                                               'School Name':'unique'}).reset_index()
+#look at grade 3 ELA
+full3ela = full[(full['Grade'] == 3) & (full['Test Id'] == 1)] 
 
-#graph, CAN I COLOR BY COUNTY?
-x = group_test['Grade']
-y = group_test['Percentage Standard Met and Above']
+#notice duplicated, drop them
+full3ela = full3ela.drop_duplicates(subset=['County Code','District Code', 'School Code', 
+                                'Subgroup ID', 'Grade', 'Test Id', 'Students Tested', 
+                                'Percentage Standard Met and Above'], keep="first", inplace=False)
+
+#by district name and graph
+by_district3ela = full3ela.groupby(['District Name']).agg({'Students Tested':'sum', 
+                                                    'Percentage Standard Met and Above': 'mean'}).reset_index()
+x = by_district3ela['District Name']
+y = by_district3ela['Percentage Standard Met and Above']
 fig, ax = plt.subplots(figsize = (18, 9))
+plt.xticks(rotation=90)
+ax.scatter(x, y, color = 'green');
 
-ax.scatter(x, y);
+#by county and graph
+by_county3ela = full3ela.groupby(['County Name']).agg({'Students Tested':'sum', 
+                                                    'Percentage Standard Met and Above': 'mean'}).reset_index()
+x = by_county3ela['County Name']
+y = by_county3ela['Percentage Standard Met and Above']
+fig, ax = plt.subplots(figsize = (10, 6))
+plt.xticks(rotation=90)
+ax.scatter(x, y, color = 'green');
+
+
+#Below worked! Did not use...
+# group_test = top10.groupby(['District Code', 'Grade']).agg({'Students Tested':'sum', 
+#                                                                'Percentage Standard Met and Above': 'mean', 
+#                                                                'County Name':'unique',
+#                                                                'District Name':'unique',
+#                                                                'School Name':'unique'}).reset_index()
