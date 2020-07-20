b8f2ead5f49583e823dce5bd80274841e6766901
mollyincali <mollyehardy@gmail.com>
2020-07-20 10:52:55 -0700

    Updated progress

--- eda_code.py
+++ eda_code.py
@@ -9,31 +9,32 @@ df = pd.read_csv("sb_ca2019_all_csv_v4.txt")
 df = df[df['Grade'] < 9]
 
 #new df with wanted columns 2456071 rows
-sbac = df.iloc[:, [0, 1, 2, 5, 6, 9, 10, 12, 16]]
+sbac = df.iloc[:, [0, 1, 2, 5, 9, 10, 12, 16]]
 
-#removes * 1273325 rows
+#remove unwanted *
 sbac = sbac[sbac['Percentage Standard Met and Above'] != '*']
+#look at just "all student"
+sbac = sbac[sbac['Subgroup ID'] == 1]
+
 #converts string to float
+sbac['Students Tested'] = sbac['Students Tested'].astype(float)
 sbac['Percentage Standard Met and Above'] = sbac['Percentage Standard Met and Above'].astype(float)
 
 #import districts
 district_code = pd.read_csv("districts.csv")
-
-#bring together sbac and district 
-full = sbac.merge(district_code, how = 'inner')
+district = district.iloc[:, [0, 1, 5, 6]]
 
 #pull only top 10 county
-top10 = full[(full['County Code'] == 18) | (full['County Code'] == 36) | (full['County Code'] == 29)
-                | (full['County Code'] == 32) | (full['County Code'] == 35) | (full['County Code'] == 43)
-                | (full['County Code'] == 1) | (full['County Code'] == 33) | (full['County Code'] == 6)
-                | (full['County Code'] == 9)]
-
-#Drop district code 0, county office scores 264158 rows
+top10 = district[(district['County Code'] == 18) | (district['County Code'] == 36) | (district['County Code'] == 29)
+                | (district['County Code'] == 32) | (district['County Code'] == 35) | (district['County Code'] == 43)
+                | (district['County Code'] == 1) | (district['County Code'] == 33) | (district['County Code'] == 6)
+                | (district['County Code'] == 9)]
 top10 = top10[top10['District Code'] != 0]
 #or top10 = top10.drop(top10[top10['District Code'] == 0].index)
 
-#Look at subgroup 'all students' in top10 12508 rows
-top10all = top10[top10['Subgroup ID'] == 1]
+#bring together sbac and district look at only all student group 381548 rows
+full = sbac.merge(top10, how = 'inner')
+full = full[full['Subgroup ID'] == 1]
 
 #Below worked! Look over again
 group_test = top10all.groupby(['District Code', 'Grade']).agg({'Students Tested':'sum', 
