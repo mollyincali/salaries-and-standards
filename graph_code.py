27968d5c8eb29fa3ce435217eb1c63de926c2948
mollyincali <mollyehardy@gmail.com>
2020-07-20 15:48:08 -0700

    Updating progress

--- graph_code.py
+++ graph_code.py
@@ -1,22 +1,22 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-import eda_code 
+from full_eda import *
 
-#by district name and graph
-by_district3ela = full3ela.groupby(['District Name']).agg({'Students Tested':'sum', 
-                                                    'Percentage Standard Met and Above': 'mean'}).reset_index()
-x = by_district3ela['District Name']
-y = by_district3ela['Percentage Standard Met and Above']
-fig, ax = plt.subplots(figsize = (18, 9))
-plt.xticks(rotation=90)
-ax.scatter(x, y, color = 'green');
+#graph grade 3 ela and math
+x = full3ela['District Name']
+y = full3ela['Percentage Standard Met and Above']
+y2 = full3math['Percentage Standard Met and Above']
 
-#by county and graph
-by_county3ela = full3ela.groupby(['County Name']).agg({'Students Tested':'sum', 
-                                                    'Percentage Standard Met and Above': 'mean'}).reset_index()
-x = by_county3ela['County Name']
-y = by_county3ela['Percentage Standard Met and Above']
 fig, ax = plt.subplots(figsize = (10, 6))
 plt.xticks(rotation=90)
-ax.scatter(x, y, color = 'green');
+ax.scatter(x, y, color = 'green')
+ax.scatter(x, y2, color = 'pink');
+
+#graph salary
+x = full3ela['District Name']
+y2 = full3ela['avg']
+
+fig, ax = plt.subplots(figsize = (10, 6))
+plt.xticks(rotation=90)
+ax.bar(x, y2, color = 'blue');
