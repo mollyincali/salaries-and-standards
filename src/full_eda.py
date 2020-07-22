126835374c46ac2e3a3441b3cba1bffa5080ba1f
mollyincali <mollyehardy@gmail.com>
2020-07-22 11:26:25 -0700

    Updating to include some dropped avg salaries

--- src/full_eda.py
+++ src/full_eda.py
@@ -13,13 +13,13 @@ sbac = sbac[sbac['Grade'] <= 11]
 #converts string to float
 sbac['Students Tested'] = sbac['Students Tested'].astype(float)
 sbac['Met or Above'] = sbac['Percentage Standard Met and Above'].astype(float)
-sbac = sbac.iloc[:, [0, 1, 2, 5, 9, 10, 12, 32]] 
+sbac = sbac[['County Code', 'School Code', 'Subgroup ID', 'Grade', 'Test Id', 'Students Tested', 'Met or Above']] 
 
 
 #---- DISTRICT CODES
 #import districts and clean
 district = pd.read_csv("../data/districts.csv") 
-district = district.iloc[:, [0, 1, 5, 6]] 
+district = district[['County Code', 'County Name', 'District Name']]
 #merge district and sbac to get names
 district_sbac = pd.merge(sbac, district, how = 'inner')
 
@@ -37,5 +37,10 @@ teacher = teacher.iloc[:,[0, 12, 13]]
 full = pd.merge(district_sbac, teacher, how = 'inner')
 full = full[full['School Code'] == 0]
 full = full.drop_duplicates(subset=['County Code','District Code', 'School Code', 
-                                'Subgroup ID', 'Grade', 'Test Id', 'Students Tested', 
-                                'Met or Above'], keep="first", inplace=False)
+                        'Subgroup ID', 'Grade', 'Test Id', 'Students Tested', 
+                        'Meclear
+                        t or Above'], keep="first", inplace=False)
+
+#----   FULL DF WITH SPECIFIC TESTS
+fullela = full[full['Test Id'] == 1]
+fullmath = full[full['Test Id'] == 2]
---
+++ src/full_eda2.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+#---- SBAC DATA
+#Import all schools all district and clean
+sbac = pd.read_csv("./data/sb_ca2019_all_csv_v4.txt").set_index('District Code')
+sbac = sbac[sbac['Percentage Standard Met and Above'] != '*']
+sbac = sbac[sbac['Subgroup ID'] == 1]
+sbac = sbac[sbac['Grade'] <= 11]
+#When school code == 0 shares report for entire district
+sbac = sbac[sbac['School Code'] == 0]
+
+#converts string to float
+sbac['Students Tested'] = sbac['Students Tested'].astype(float)
+sbac['Met or Above'] = sbac['Percentage Standard Met and Above'].astype(float)
+sbac = sbac[['County Code', 'Grade', 'Test Id', 'Students Tested', 'Met or Above']].drop_duplicates()
+
+
+#---- DISTRICT CODES
+#import districts and clean
+district = pd.read_csv("./data/districts.csv").set_index('District Code')
+district = district[['County Code', 'County Name', 'District Name']].drop_duplicates()
+
+#merge district and sbac to get names
+district_sbac = district.join(sbac, lsuffix = '_district', how = 'inner')
+
+
+#---- TEACHER CODES
+teacher = pd.read_csv("./data/salary.csv")
+teacher["avg"] = teacher['Average Salary Paid'].str.replace(',','').astype(float)
+teacher['District Name'] = teacher['LEA']
+teacher = teacher.dropna()
+teacher = teacher[['District Name', 'avg', 'Co']].drop_duplicates()
+
+#Noticed not all District Names match up, trying to catch all
+teacher2 = teacher.copy()
+teacher2['District Name'] = teacher2['District Name'].str.replace(' Elementary', '')
+teacher2['District Name'] = teacher2['District Name'].str.replace(' High', '')
+teacher2['District Name'] = teacher2['District Name'].str.replace(' Elem.', '')
+
+#Bring two df together
+teacher = pd.concat([teacher, teacher2]).drop_duplicates().set_index(['District Name'])
+
+#---- FULL & FINAL DATA FRAME
+test1 = district_sbac.reset_index().set_index('District Name').join(teacher, how = 'inner')
