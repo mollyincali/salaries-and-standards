8f36e7d633415aea463330aec69e274373fb9182
mollyincali <mollyehardy@gmail.com>
2020-07-22 11:53:47 -0700

    Updating with r values

--- src/stat_testing.py
+++ src/stat_testing.py
@@ -2,9 +2,25 @@ import pandas as pd
 import seaborn as sn
 import matplotlib.pyplot as plt
 
-#pearsonr test return pearson's correlation, p-value
-from scipy import stats
-x = np.array(temp['avg'])
-y = np.array(temp['Met or Above'])
+#--- R using Pandas
+full['Met or Above'].corr(full['avg'])
 
-stats.pearsonr(x, y)
+
+#--- R using Pandas by county
+r = []
+for i in range(1, 59):
+    temp_df = full[full['Co'] == i][['avg','Met or Above']]
+    temp_r = temp_df['Met or Above'].corr(temp_df['avg'])
+    r.append([i, temp_r])
+
+r_df = pd.DataFrame(r, columns = ['Co', 'r']).sort_values(by = 'r').dropna()
+
+
+
+#---- THROW AWAY?
+# #pearsonr test return pearson's correlation, p-value
+# from scipy import stats
+# x = np.array(temp['avg'])
+# y = np.array(temp['Met or Above'])
+
+# stats.pearsonr(x, y)
